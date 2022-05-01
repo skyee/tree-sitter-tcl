26990fdfe7dc99c7eda6f97c811e5fd1bd7ee8fa
Cezary Dro?ak <cezary@drozak.net>
2022-05-01 10:47:10 +0200

    Add elseif_clause rule

--- tclsh/grammar.js
+++ tclsh/grammar.js
@@ -7,7 +7,8 @@ module.exports = grammar(tclGrammar, {
 
     _builtin_command: $ => choice($.if_command),
 
-    if_command: $ => seq($.if_clause, optional($.else_clause)),
+    if_command: $ =>
+      seq($.if_clause, repeat($.elseif_clause), optional($.else_clause)),
 
     if_clause: $ =>
       seq(
@@ -16,7 +17,8 @@ module.exports = grammar(tclGrammar, {
         optional('then'),
         field('body', $.braced_word),
       ),
-
+    elseif_clause: $ =>
+      seq('elseif', field('condition', $._word), field('body', $.braced_word)),
     else_clause: $ => seq(optional('else'), field('body', $.braced_word)),
   },
 })
