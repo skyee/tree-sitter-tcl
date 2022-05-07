998cf090e5bb7343dfd0b9288972ee3dd61c4712
Cezary Dro?ak <cezary@drozak.net>
2022-05-07 19:52:21 +0200

    Add task for uploading to git repository

---
+++ scripts/upload_to_git.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# NOTE: This script is meant to be run from the .build.yml file. It requires to
+# be run from the root of the repository and to have tree-sitter-tcl-pregen
+# directory in the same directory as tree-sitter-tcl.
+set -euo pipefail
+
+commit_num=$(hg identify --num)
+commit_id=$(hg identify --id)
+revision="r.$commit_num.$commit_id"
+message=$(
+	cat <<-EOF
+		Add revision $revision
+
+		See: https://hg.sr.ht/~cdrozak/tree-sitter-tcl/rev/$revision
+	EOF
+)
+
+cp -rf \
+	Cargo.{lock,toml} \
+	binding.gyp \
+	bindings \
+	package-lock.json \
+	package.json \
+	tcl{,sh}/{package.json,src} \
+	tclsh/queries \
+	../tree-sitter-tcl-pregen
+
+cd ../tree-sitter-tcl-pregen
+git add .
+git commit --message "$message"
